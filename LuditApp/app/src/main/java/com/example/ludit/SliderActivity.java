3fe7f871133178399e5e46f08132aff7107d8d9c
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-09-23 09:06:03 -0300

    TabLayout
    Utilização de tabLayout para indicar páginas do view pager

--- LuditApp/app/src/main/java/com/example/ludit/SliderActivity.java
+++ LuditApp/app/src/main/java/com/example/ludit/SliderActivity.java
@@ -5,18 +5,27 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class SlideActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class SliderActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private TabLayout tabLayout;
     private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slide);
+        setContentView(R.layout.activity_slider);
 
-        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
+
+        tabLayout.setupWithViewPager(viewPager, true);
     }
+
+
 }
