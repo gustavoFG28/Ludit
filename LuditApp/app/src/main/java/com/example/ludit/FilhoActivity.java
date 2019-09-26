151df8e277541c0c26673f5d36ccbacee9282ab2
GUSTAVO FERREIRA GITZEL <u18194@academico.cotuca.unicamp.br>
2019-09-26 11:54:51 -0300

    SharedPreferences

---
+++ LuditApp/app/src/main/java/com/example/ludit/FilhoActivity.java
@@ -0,0 +1,14 @@
+package com.example.ludit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filho);
+    }
+}
--- LuditApp/app/src/main/java/com/example/ludit/MainActivity.java
+++ LuditApp/app/src/main/java/com/example/ludit/MainActivity.java
@@ -3,51 +3,23 @@ package com.example.ludit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnJogos, btnMusicas, btnVideos, btnLivros;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnJogos = (Button)findViewById(R.id.btnJogos);
-        btnMusicas = (Button) findViewById(R.id.btnMusicas);
-        btnVideos = (Button) findViewById(R.id.btnVideos);
-        btnLivros = (Button) findViewById(R.id.btnLivros);
-
-        btnJogos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btnMusicas.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                trocarPagina(MusicaActivity.class);
-            }
-        });
-
-        btnLivros.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        btnVideos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                trocarPagina(SliderActivity.class);
-            }
-        });
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        if(sharedPreferences.contains("email"))
+            trocarPagina(FilhoActivity.class);
+        else
+            trocarPagina(SliderActivity.class);
 
     }
 
