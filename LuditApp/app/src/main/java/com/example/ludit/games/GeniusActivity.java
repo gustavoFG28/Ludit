c63adee47843ef02cc1f7acd034c82b86e8b307e
GabrielleBarbosa <42346058+GabrielleBarbosa@users.noreply.github.com>
2019-11-22 10:56:26 -0300

    Update GeniusActivity.java

--- LuditApp/app/src/main/java/com/example/ludit/games/GeniusActivity.java
+++ LuditApp/app/src/main/java/com/example/ludit/games/GeniusActivity.java
@@ -27,8 +27,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class GeniusActivity extends AppCompatActivity {
-   // Button btnAzul, btnVermelho, btnAmarelo, btnVerde;
-   // Button[] btns = new Button[4];
     Handler handler;
     ConnectionThread thread;
 
@@ -48,25 +46,13 @@ public class GeniusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_genius);
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_IMMERSIVE
-                        // Set the content to appear under the system bars so that the
-                        // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        // Hide the nav bar and status bar
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
 
-        /*btnAmarelo = (Button) findViewById(R.id.btnAmarelo);
-        btnAzul = (Button) findViewById(R.id.btnAzul);
-        btnVerde = (Button) findViewById(R.id.btnVerde);
-        btnVermelho = (Button) findViewById(R.id.btnVermelho);
-
-        btns[0] = btnAmarelo;  cores[0] = Color.parseColor("#f57f17");
-        btns[1] = btnAzul;     cores[1] = Color.parseColor("#01579b");
-        btns[2] = btnVermelho; cores[2] = Color.parseColor("#b71c1c");
-        btns[3] = btnVerde;    cores[3] = Color.parseColor("#1b5e20");*/
 
         cores[0] = R.drawable.genius_vermelho;
         cores[1] = R.drawable.genius_azul;
@@ -195,6 +181,7 @@ public class GeniusActivity extends AppCompatActivity {
                 }).start();
             }
         };
+
         Runnable r  = new Runnable() {
             @Override
             public void run() {
@@ -215,7 +202,7 @@ public class GeniusActivity extends AppCompatActivity {
     }
 
     public void  atualizarTela() {
-        if(qtd < max) {
+        if(qtd < max-1) {
             qtd++;
             if(max == 5)
                 pontosGenius++;
@@ -223,6 +210,14 @@ public class GeniusActivity extends AppCompatActivity {
                 pontosGenius += 0.5f;
             construirGenius();
         }else{
+
+            if(qtd < max)
+            {
+                if(max == 5)
+                    pontosGenius++;
+                else
+                    pontosGenius += 0.5f;
+            }
 
             thread.cancel();
 
@@ -233,26 +228,21 @@ public class GeniusActivity extends AppCompatActivity {
             else if(pontosGenius == 3|| pontosGenius == 2) pontoFinal = 0.05f;
             else if(pontosGenius == 4 || pontosGenius == 5) pontoFinal = 0.1f;
 
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(GeniusActivity.this);
+            AlertDialog alerta;
+            builder.setTitle("Sua pontuação foi de "+ pontosGenius);
+            builder.setMessage("PARABÉNS, DEU CERTO");
+            builder.setNegativeButton("OK", null);
+            alerta = builder.create();
+            alerta.show();
+
             UserService service =  RetrofitConfig.getClient().create(UserService.class);
-
             Call<List<Filho>> ponto = service.skill(email,nomeFilho,"mem", pontoFinal);
-
             ponto.enqueue(new Callback<List<Filho>>() {
                 @Override
                 public void onResponse(Call<List<Filho>> call, Response<List<Filho>> response) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(GeniusActivity.this);
 
-                    AlertDialog alerta;
-
-                    builder.setTitle("Sua pontuação foi de "+ pontosGenius);
-
-                    builder.setMessage("PARABÉNS, DEU CERTO");
-
-                    builder.setNegativeButton("OK", null);
-
-                    alerta = builder.create();
-
-                    alerta.show();
                 }
 
                 @Override
